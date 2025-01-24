f4e76e91074ad26e1efd42f2efc36319a08a724a
mihnea <nmihneaconst.ieu2023@student.ie.edu>
2025-01-24 12:22:23 +0100

    Inputs

--- Session5.py
+++ Session5.py
@@ -1,11 +1,36 @@
-name = input("Como te llamas?")
-# print("Hola", name)
-# age = input("How old are you "+name+"? ")
-# print(name+ "were you born in?"+str(2024-input(age)))
 
-age2 = input(f"How old are you {name}?")
-age2 = int(age2)
-print(f"{name}, where you born in {2024-age2}?")
+name = input("Como te llamas? ")
+if name == "Max":
+    print("Who let you out of your cage ")
+else:
+    repeat = True
+    while repeat:
+        age2 = input(f"How old are you {name}? ")
+        try:
+            age2 = int(age2)
+            check = input(f"{name}, where you born in {2024-age2}? ")
+            if check == "yes":
+                print("Nice")
+                repeat = False
+            else:
+                print("you're wrong")
+                repeat = False
+        except:
+            print("Fuck you,Invalid Response")
+            print("integers only")
+            Again = input("Do you want to try again? ")
+            if Again == "yes":
+                print("Okay")
+            else:
+                print(":(")
+                repeat = False
 
+# repeat = True
+# while repeat:
+#     user_input = input("Enter 'exit' to quit: ")
+#     if user_input == "exit":
+#         repeat = False
+#     else:
+#         print("You entered:", user_input)
 
 
