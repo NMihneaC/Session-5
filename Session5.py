913692c93f983f87522e992e06990496cac38c17
mihnea <nmihneaconst.ieu2023@student.ie.edu>
2025-01-24 11:26:55 +0100

    Inputs

---
+++ Session5.py
@@ -0,0 +1,11 @@
+name = input("Como te llamas?")
+# print("Hola", name)
+# age = input("How old are you "+name+"? ")
+# print(name+ "were you born in?"+str(2024-input(age)))
+
+age2 = input(f"How old are you {name}?")
+age2 = int(age2)
+print(f"{name}, where you born in {2024-age2}?")
+
+
+
